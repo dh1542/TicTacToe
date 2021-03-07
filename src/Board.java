dfa3a177fcc19af7002b5557684335c6e22bcb0c
Dominik Hoffmann <dom@hoffmann.com>
2021-03-07 14:46:35 +0100

    set up the board class

---
+++ src/Board.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Board {
+    private char[][] values;
+
+    // constructor for board
+    public Board(char[][] values){
+        setValues(values);
+        init_start(this.values);
+
+    }
+
+    public void print_newBoard(){
+        System.out.println("   0 1 2");
+        System.out.println("-------------");
+        System.out.println("0 |" + " " + values[0][0] + " " + values[1][0] + " " + values[2][0] + " |");
+        System.out.println("1 |" + " " + values[0][1] + " " + values[1][1] + " " + values[2][1] + " |");
+        System.out.println("2 |" + " " + values[0][2] + " " + values[1][2] + " " + values[2][2] + " |");
+        System.out.println("-------------");
+    }
+    public void init_start(char[][] values){
+        for (char[] row: values)
+            // 'e' for empty
+            Arrays.fill(row, 'e');
+    }
+
+    public void setValues(char[][] values) {
+        this.values = new char[3][3];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        char [][] val = new char[3][3];
+        Board b = new Board(val);
+
+        b.print_newBoard();
+
+
+
+
+
+
+    }
+}
